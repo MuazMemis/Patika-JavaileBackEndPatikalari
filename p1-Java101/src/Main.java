a215c45ce838e2ada2b7f46494342c43a3b222fa
Muaz MEMİŞ <muazmemis@gmail.com>
2021-09-15 18:47:39 +0300

    Düzenleme

--- p1-Java101/src/Main.java
+++ p1-Java101/src/Main.java
@@ -1,20 +1,4 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        int r, alfa;
-        double pi = 3.14, alan;
-
-        Scanner input = new Scanner(System.in);
-
-        System.out.print("Dairenin yarıçapını giriniz: ");
-        r = input.nextInt();
-
-        System.out.print("Açıyı giriniz: ");
-        alfa = input.nextInt();
-
-        alan = pi * r * r * alfa / 360;
-
-        System.out.println("Dairenin alanı : " + alan);
     }
 }
