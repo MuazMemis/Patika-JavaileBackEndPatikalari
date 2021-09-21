9c75264c9ae92c2589c08ca3008520f363952bb4
Muaz MEMİŞ <muazmemis@gmail.com>
2021-09-21 04:51:35 +0300

    Ders 66 bitti. Çok boyutlu diziler

--- p1-Java101/src/Main.java
+++ p1-Java101/src/Main.java
@@ -4,15 +4,31 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int[] numbers1 = new int[5];
-        numbers1[0] = 10;
-        numbers1[1] = 15;
-        numbers1[2] = 20;
-        numbers1[3] = 25;
-        numbers1[4] = 30;
+        int[][] uzaklik = {
+                {0, 453, 939, 243, 783, 891},
+                {453, 0, 490, 384, 620, 439},
+                {939, 490, 0, 839, 863, 423},
+                {243, 384, 839, 0, 544, 823},
+                {783, 620, 863, 544, 0, 1045},
+                {891, 439, 423, 823, 1045, 0}
+        };
 
-        int[] numbers2 = {10, 15, 20, 25, 30};
-        int[] numbers3 = new int[]{10, 15, 20, 25, 30};
+        int matrix[][] = new int[3][4];
+        System.out.println("Satır elemanları : " + matrix.length);
+        System.out.println("Sütun elemanları : " + matrix[0].length);
 
+        int number = 1;
+        for (int row = 0; row < matrix.length; row++) {
+            for (int column = 0; column < matrix[row].length; column++) {
+                matrix[row][column] = number++;
+            }
+        }
+        for (int row = 0; row < matrix.length; row++) {
+            for (int column = 0; column < matrix[row].length; column++) {
+                System.out.print(matrix[row][column] + " ");
+            }
+            System.out.println();
+        }
     }
 }
+
