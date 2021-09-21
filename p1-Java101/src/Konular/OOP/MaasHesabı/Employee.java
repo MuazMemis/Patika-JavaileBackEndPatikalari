9a7671cd3a3fe6a41bf1b199d3f23fb99b1d3624
Muaz MEMİŞ <muazmemis@gmail.com>
2021-09-21 03:42:48 +0300

    Edit public

--- p1-Java101/src/Konular/OOP/MaasHesabı/Employee.java
+++ p1-Java101/src/Konular/OOP/MaasHesabı/Employee.java
@@ -2,7 +2,7 @@ package Konular.OOP.MaasHesabı;
 
 public class Employee {
     String name;
-    public double salary;
+    double salary;
     int workHours;
     int hireYear;
 
@@ -26,14 +26,14 @@ public class Employee {
         System.out.println("Toplam Maaş : " + totalSalary());
     }
 
-    public double tax() {
+    double tax() {
         if (this.salary > 1000) {
             return this.salary * 0.03;
         }
         return 0;
     }
 
-    public double bonus() {
+    double bonus() {
         if (this.workHours > 40) {
             int overtime = this.workHours - 40;
             return overtime * 30;
@@ -41,7 +41,7 @@ public class Employee {
         return 0;
     }
 
-    public double raiseSalary() {
+    double raiseSalary() {
         int workYear = 2021 - this.hireYear;
 
         if (workYear < 10) {
@@ -55,15 +55,14 @@ public class Employee {
         return 0;
     }
 
-    public double totalSalary() {
+    double totalSalary() {
         return this.salary - tax() + bonus() + raiseSalary();
     }
 
-    public void employeeInfo() {
+    void employeeInfo() {
         System.out.println("Adı : " + this.name);
         System.out.println("Maaşı : " + this.salary);
         System.out.println("Çalışma Saati : " + this.workHours);
         System.out.println("Başlangıç Yılı : " + this.hireYear);
     }
-
 }
