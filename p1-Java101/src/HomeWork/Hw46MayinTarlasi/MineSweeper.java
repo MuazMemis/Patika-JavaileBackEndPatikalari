7c49842e5ddb176c318ba25d079aab5e845440ce
Muaz MEMİŞ <muazmemis@gmail.com>
2021-09-24 21:44:38 +0300

    Mayın Tarlası GUI Swing ile yapıldı.

--- p1-Java101/src/HomeWork/Hw46MayinTarlasi/MineSweeper.java
+++ p1-Java101/src/HomeWork/Hw46MayinTarlasi/MineSweeper.java
@@ -8,8 +8,11 @@ import java.awt.event.MouseListener;
 public class MineSweeper implements MouseListener {
     JFrame frame;
     Btn[][] board = new Btn[10][10];
+    int openButton;
 
     public MineSweeper() {
+        openButton = 0;
+
         frame = new JFrame("Mayın Tarlası");
         frame.setSize(800, 800);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -25,9 +28,10 @@ public class MineSweeper implements MouseListener {
         }
 
         generateMine();
-        print();
-        frame.setVisible(true);
+        updateCount();
+        printMine();
 
+        frame.setVisible(true);
     }
 
     public void generateMine() {
@@ -50,8 +54,62 @@ public class MineSweeper implements MouseListener {
             for (int col = 0; col < board[0].length; col++) {
                 if (board[row][col].isMine()) {
                     board[row][col].setIcon(new ImageIcon("src/Assets/mine.png"));
+                } else {
+                    board[row][col].setText(board[row][col].getCount() + "");
+                    board[row][col].setEnabled(false);
                 }
             }
+        }
+    }
+
+    public void printMine(){
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[0].length; col++) {
+                if (board[row][col].isMine()) {
+                    board[row][col].setIcon(new ImageIcon("src/Assets/mine.png"));
+                }
+            }
+        }
+    }
+
+    public void updateCount() {
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[0].length; col++) {
+                if (board[row][col].isMine()) {
+                    counting(row, col);
+                }
+            }
+        }
+    }
+
+    public void counting(int row, int col) {
+        for (int i = row - 1; i <= row + 1; i++) {
+            for (int j = col - 1; j <= col + 1; j++) {
+                try {
+                    int value = board[i][j].getCount();
+                    board[i][j].setCount(++value);
+                } catch (Exception e) {
+
+                }
+            }
+        }
+    }
+
+    public void open(int r, int c) {
+        if (r < 0 || r >= board.length || c < 0 || c >= board[0].length || board[r][c].getText().length() > 0
+                || board[r][c].isEnabled() == false) {
+            return;
+        } else if (board[r][c].getCount() != 0) {
+            board[r][c].setText(board[r][c].getCount() + "");
+            board[r][c].setEnabled(false);
+            openButton++;
+        } else {
+            openButton++;
+            board[r][c].setEnabled(false);
+            open(r - 1, c);
+            open(r + 1, c);
+            open(r, c - 1);
+            open(r, c + 1);
         }
     }
 
@@ -59,9 +117,17 @@ public class MineSweeper implements MouseListener {
     public void mouseClicked(MouseEvent e) {
         Btn b = (Btn) e.getComponent();
         if (e.getButton() == 1) {
-            System.out.println("sol tık");
+            if (b.isMine()) {
+                JOptionPane.showMessageDialog(frame, "Mayına bastınız. Oyun bitti.");
+                print();
+            } else {
+                open(b.getRow(), b.getCol());
+                if (openButton == (board.length * board[0].length) - 10) {
+                    JOptionPane.showMessageDialog(frame, "Tebrikler. Kazandınız.");
+                    print();
+                }
+            }
         } else if (e.getButton() == 3) {
-            System.out.println("sağ tık");
             if (!b.isFlag()) {
                 b.setIcon(new ImageIcon("src/Assets/flag.png"));
                 b.setFlag(true);
