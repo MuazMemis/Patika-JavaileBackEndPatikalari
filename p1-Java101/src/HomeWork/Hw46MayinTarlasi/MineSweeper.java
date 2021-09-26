85033d67e2e5e7b0e72e1a6b3eb23aac228d8d83
Muaz MEMİŞ <muazmemis@gmail.com>
2021-09-26 19:35:07 +0300

    Mine Sweeper Ödevi bitti.

--- p1-Java101/src/HomeWork/Hw46MayinTarlasi/MineSweeper.java
+++ p1-Java101/src/HomeWork/Hw46MayinTarlasi/MineSweeper.java
@@ -1,47 +1,112 @@
 package HomeWork.Hw46MayinTarlasi;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class MineSweeper {
-    int rowNumber;
-    int colNumber;
+    int rowNumber, colNumber, size, mineCount;
+    int[][] map;
+    int[][] board;
+    boolean game = true;
+
+    Random random = new Random();
+    Scanner scan = new Scanner(System.in);
 
     public MineSweeper(int rowNumber, int colNumber) {
         this.rowNumber = rowNumber;
         this.colNumber = colNumber;
+        this.map = new int[rowNumber][colNumber];
+        this.board = new int[rowNumber][colNumber];
+        this.size = rowNumber * colNumber;
+        this.mineCount = size / 4;
     }
 
     public void run() {
-        String[][] matris = new String[this.rowNumber][this.colNumber];
-        int yildiz = this.rowNumber * this.colNumber / 4;
-        int sayac = 0;
-        boolean yildizBasma = false;
+        int row, col, success = 0;
+        int mineWithoutCount = this.size - this.mineCount;
 
-        Random rand = new Random();
-        int yildizColNumber = 0;
-        int yildizRowNumber = 0;
+        prepareGame();
+        System.out.println("Map");
+        print(this.map);
+        System.out.println("=============");
 
-        for (int i = 0; i < matris.length; i++) {
+        System.out.println("Oyun başladı.");
+        while (this.game) {
+            System.out.println("Mayın Tarlası");
+            print(this.board);
+            System.out.println("=============");
+            System.out.print("Satır : ");
+            row = scan.nextInt();
+            System.out.print("Sütuns : ");
+            col = scan.nextInt();
 
-            if (yildizBasma) {
-                yildizRowNumber = rand.nextInt(this.rowNumber);
-                yildizColNumber = rand.nextInt(this.colNumber);
-            }
+            if (isCoordinate(row, col)) continue;
 
-            for (int j = 0; j < matris[i].length; j++) {
-//                System.out.println("row : " + yildizRowNumber + " i :" + i);
-//                System.out.println("col : " + yildizColNumber + " j :" + j);
+            if (this.map[row][col] != -1) {
+                check(row, col);
+                success++;
 
-                if ((i == yildizRowNumber) && (j == yildizColNumber)) {
-                    if (sayac <= yildiz) {
-                        System.out.print("* ");
-                        yildizBasma = true;
-                        sayac++;
-                    }
-                } else {
-                    System.out.print("- ");
+                if (success == (mineWithoutCount)) {
+                    System.out.println("Başardınız!");
+                    break;
                 }
+            } else {
+                this.game = false;
+                System.out.println("Game Over!");
+            }
+        }
+    }
 
+    private boolean isCoordinate(int row, int col) {
+        if (row < 0 || row >= this.rowNumber || col < 0 || col >= this.colNumber) {
+            System.out.println("Geçersiz koordinat.");
+            return true;
+        }
+        return false;
+    }
+
+    private void check(int row, int col) {
+        if (this.map[row][col] == 0) {
+            if ((col < this.colNumber - 1) && (this.map[row][col + 1] == -1)) {
+                this.board[row][col]++;
+            }
+            if ((col > 0) && (this.map[row][col - 1] == -1)) {
+                this.board[row][col]++;
+            }
+            if ((row < this.rowNumber - 1) && (this.map[row + 1][col] == -1)) {
+                this.board[row][col]++;
+            }
+            if ((row > 0) && (this.map[row - 1][col] == -1)) {
+                this.board[row][col]++;
+            }
+            if (this.board[row][col] == 0) {
+                this.board[row][col] = -2;
+            }
+        }
+    }
+
+    private void prepareGame() {
+        int randRow, randCol, count = 0;
+
+        while (count != (this.mineCount)) {
+            randRow = this.random.nextInt(this.rowNumber);
+            randCol = this.random.nextInt(this.colNumber);
+
+            if (this.map[randRow][randCol] != -1) {
+                this.map[randRow][randCol] = -1;
+                count++;
+            }
+        }
+    }
+
+    private void print(int[][] arr) {
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[0].length; col++) {
+                if (arr[row][col] >= 0) {
+                    System.out.print(" " + arr[row][col] + " ");
+                } else {
+                    System.out.print(arr[row][col] + " ");
+                }
             }
             System.out.println();
         }
--- p1-Java101/src/Main.java
+++ p1-Java101/src/Main.java
@@ -1,8 +1,25 @@
+import HomeWork.Hw46MayinTarlasi.MineSweeper;
 import HomeWork.Hw46MayinTarlasi.MineSweeper2;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        MineSweeper2 mine = new MineSweeper2();
+        Scanner scan = new Scanner(System.in);
+        int row, col;
+
+        System.out.println("Mayın Tarlasına Hoşgeldiniz!");
+        System.out.println("Lütfen oynamak istediğiniz boyutları giriniz.");
+
+        System.out.print("Satır sayısı : ");
+        row = scan.nextInt();
+
+        System.out.print("Sütun sayısı : ");
+        col = scan.nextInt();
+
+        MineSweeper mine = new MineSweeper(row, col);
+        mine.run();
+//        MineSweeper2 mine2 = new MineSweeper2();
     }
 }
 
