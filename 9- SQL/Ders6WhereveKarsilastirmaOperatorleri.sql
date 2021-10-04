7c6b2ef1e2f111346ff0fa2aea1aa2853a51f66b
Muaz MEMİŞ <muazmemis@gmail.com>
2021-10-04 21:52:40 +0300

    Create Ders6WhereveKarsilastirmaOperatorleri.sql

---
+++ 9- SQL/Ders6WhereveKarsilastirmaOperatorleri.sql
@@ -0,0 +1,8 @@
+--SELECT * FROM film Where replacement_cost = 14.99;
+--SELECT * FROM film Where rental_rate = 0.99;
+--SELECT title, length FROM film Where rental_rate = 0.99;
+--SELECT * FROM actor Where first_name = 'Penelope';
+--SELECT * FROM film Where length > 99;
+--SELECT * FROM film Where rental_rate <= 1;
+--SELECT * FROM film Where rental_rate != 0.99;
+SELECT * FROM film Where rental_rate <> 4.99;
