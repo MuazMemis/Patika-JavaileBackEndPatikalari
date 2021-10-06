f17eb2cb5cbe2373ff1f50856cac0d779e4eb94c
Muaz MEMİŞ <muazmemis@gmail.com>
2021-10-06 14:11:07 +0300

    Create Ders16Having.sql

---
+++ Bolum9SQL/Ders16Having.sql
@@ -0,0 +1,7 @@
+SELECT rental_rate, COUNT(*) FROM film GROUP BY rental_rate HAVING COUNT(*) > 325;
+SELECT staff_id, COUNT(*) FROM payment GROUP BY staff_id HAVING COUNT(*) > 7300;
+SELECT customer_id, SUM(amount) FROM payment 
+GROUP BY customer_id 
+HAVING SUM(amount) > 100
+ORDER BY SUM(amount) DESC
+LIMIT 1;
