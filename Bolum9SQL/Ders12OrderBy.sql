4f30c739f89cd755692d908dafe567dde460efb2
Muaz MEMİŞ <muazmemis@gmail.com>
2021-10-06 11:52:53 +0300

    Ders 12 bitti.

---
+++ Bolum9SQL/Ders12OrderBy.sql
@@ -0,0 +1,7 @@
+SELECT * FROM film ORDER BY title;
+SELECT * FROM film ORDER BY title ASC;
+SELECT * FROM film ORDER BY title DESC;
+SELECT title, length FROM film ORDER BY length DESC;
+SELECT title, rental_rate, length FROM film ORDER BY rental_rate ASC, length DESC;
+SELECT title, rental_rate, length FROM film WHERE title LIKE 'A%' ORDER BY rental_rate ASC, length DESC;
+SELECT * FROM film WHERE rating = 'G' ORDER BY length DESC;
