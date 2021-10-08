105cbdf128973cbd1005845c89b5342f00ea724c
Muaz MEMİŞ <muazmemis@gmail.com>
2021-10-08 10:27:28 +0300

    Static Kod Blokları bitti.

---
+++ Bolum10Java102/java102/src/Bolum1AccessModifier/Ders3StaticKodBloklari/Main.java
@@ -0,0 +1,12 @@
+package Bolum1AccessModifier.Ders3StaticKodBloklari;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("1. Program Başladı !");
+        User u1 = new User("Muaz");
+        User u2 = new User("Patika");
+
+        System.out.println("Counter Son Değer : " + User.counter);
+        System.out.println("Program Bitti !");
+    }
+}
