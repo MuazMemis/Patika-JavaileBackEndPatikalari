4f4d670ab4da5e167e215e97e1c352cdc9820f6e
Muaz MEMİŞ <muazmemis@gmail.com>
2021-10-08 11:06:15 +0300

    Final Anahtar Sözcüğü ve Sabit Tanımlama

---
+++ Bolum10Java102/java102/src/Dersler/Bolum1AccessModifier/Ders4FinalAnahtarSozcugu/Main.java
@@ -0,0 +1,8 @@
+package Dersler.Bolum1AccessModifier.Ders4FinalAnahtarSozcugu;
+
+public class Main {
+    public static void main(String[] args) {
+        Daire d = new Daire(10);
+        d.calcArea();
+    }
+}
