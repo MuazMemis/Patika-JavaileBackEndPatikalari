40d316077915b53a6a92a5c75ecf711ae5949dbc
Muaz MEMİŞ <muazmemis@gmail.com>
2021-10-13 19:36:08 +0300

    TreeSet

---
+++ Bolum10-Java102/java102/src/Dersler/Bolum6CollectionveMapInterface/Ders4TreeSet/Main.java
@@ -0,0 +1,24 @@
+package Dersler.Bolum6CollectionveMapInterface.Ders4TreeSet;
+
+import java.util.TreeSet;
+
+public class Main {
+    public static void main(String[] args) {
+//        TreeSet<Student> students = new TreeSet<>(new OrderNoteComparator());
+//        TreeSet<Student> students = new TreeSet<>(new OrderNoteComparator().reversed()); // büyükten küçüğe sıralar.
+//        TreeSet<Student> students = new TreeSet<>(new OrderNameComparator());
+        TreeSet<Student> students = new TreeSet<>(new OrderNameComparator().reversed());
+
+        students.add(new Student("Mustafa", 20));
+        students.add(new Student("Ahmet", 100));
+        students.add(new Student("Bekir", 40));
+        students.add(new Student("Hasan", 50));
+        students.add(new Student("Hasan", 60));
+        students.add(new Student("Cenk", 70));
+
+        for (Student student : students) {
+            System.out.println(student.getName() + "\t : " + student.getNote());
+        }
+
+    }
+}
