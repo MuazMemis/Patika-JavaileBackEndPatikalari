9320a48b162ff456b7eb45c6d76f0e0c086196f6
Muaz MEMİŞ <muazmemis@gmail.com>
2021-10-15 04:55:30 +0300

    Veritabanı İşlemleri

---
+++ Bolum10-Java102/java102/src/Dersler/Bolum8VeriTabaniIslemleriJDBC/Ders3VeritabaniIslemleri/Main.java
@@ -0,0 +1,11 @@
+package Dersler.Bolum8VeriTabaniIslemleriJDBC.Ders3VeritabaniIslemleri;
+
+public class Main {
+    public static final String DB_URL = "jdbc:mysql://localhost/dbName";
+    public static final String DB_USERNAME = "postgres";
+    public static final String DB_PASSWORD = "togg";
+
+    public static void main(String[] args) {
+
+    }
+}
