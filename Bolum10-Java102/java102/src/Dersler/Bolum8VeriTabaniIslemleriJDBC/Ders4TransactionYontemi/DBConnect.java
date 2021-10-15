d095b2bbfbf35a54161b23dcd187b5b7acbaab64
Muaz MEMİŞ <muazmemis@gmail.com>
2021-10-15 05:07:39 +0300

    Transaction Yöntemi

---
+++ Bolum10-Java102/java102/src/Dersler/Bolum8VeriTabaniIslemleriJDBC/Ders4TransactionYontemi/DBConnect.java
@@ -0,0 +1,31 @@
+package Dersler.Bolum8VeriTabaniIslemleriJDBC.Ders4TransactionYontemi;
+
+import java.sql.*;
+
+public class DBConnect {
+    private static final String DB_URL = "jdbc:postgresql://localhost:5432/univercity";
+    private static final String DB_USERNAME = "postgres";
+    private static final String DB_PASSWORD = "togg";
+
+    public static void main(String[] args) throws SQLException {
+        Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+        connection.setAutoCommit(false);
+
+        PreparedStatement pr = connection.prepareStatement("INSERT INTO student (student_name, student_class) VALUES (?, ?)");
+        pr.setString(1, "Jhon");
+        pr.setInt(2, 1);
+        pr.executeUpdate();
+
+        if (true) {
+            throw new SQLException();
+        }
+
+        pr.setString(1, "Doe");
+        pr.setInt(2, 3);
+        pr.executeUpdate();
+
+        connection.commit();
+        pr.close();
+        connection.close();
+    }
+}
