86723ab69a6e7eec1d158e06ebe899432a6051c4
Muaz MEMİŞ <muazmemis@gmail.com>
2022-05-03 06:09:23 +0300

    Spring Framework 10 cdi @New

---
+++ 3-IleriSeviye/Bolum01-JavaSpringBoot/SpringFramework/src/main/java/com/muazmemis/StudentDto.java
@@ -0,0 +1,19 @@
+package com.muazmemis;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+// Data Transfer Object (DTO)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class StudentDto {
+
+    private Long studentId;
+    private String name;
+    private String surname;
+
+}
