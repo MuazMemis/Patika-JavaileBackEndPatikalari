843372fe8eb44a2b94c044db8d7efcb99b29440c
Muaz MEMİŞ <muazmemis@gmail.com>
2022-05-04 16:39:12 +0300

    Spring Framework 21 AOPdecorator

---
+++ 3-IleriSeviye/Bolum01-JavaSpringBoot/SpringFramework/src/main/java/com/muazmemis/aop/decorator/EvDecorator.java
@@ -0,0 +1,26 @@
+package com.muazmemis.aop.decorator;
+
+import javax.decorator.Decorator;
+import javax.decorator.Delegate;
+import javax.inject.Inject;
+
+@Decorator
+abstract public class EvDecorator implements IEv {
+
+    private @Inject
+    @Delegate
+    IEv iEv;
+
+    @Override
+    public String boyama(String data) {
+        System.out.println("Decorator Buradaydı !!!");
+        String dataInformation = iEv.boyama(data);
+
+        if (dataInformation.equals("kodluyoruz")) {
+            dataInformation = dataInformation.toUpperCase();
+        }
+
+        return dataInformation;
+    }
+
+}
