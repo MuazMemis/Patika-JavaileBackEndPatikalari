899443d5aa268aec15e59a946ce60ed8e20abfcd
Muaz MEMİŞ <muazmemis@gmail.com>
2022-05-05 01:20:34 +0300

    SpringBoot 27 @Bean @PostContruct

---
+++ 3-IleriSeviye/Bolum01-JavaSpringBoot/SpringBoot/src/main/java/com/muazmemis/bean/PostConstructTutorials.java
@@ -0,0 +1,33 @@
+package com.muazmemis.bean;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import javax.annotation.PostConstruct;
+import java.util.logging.Logger;
+
+@Controller
+public class PostConstructTutorials {
+
+    @Autowired
+    Logger LOG;
+
+    // Parametresiz constructor
+    // Cannot invoke "java.util.logging.Logger.info(String)" because "this.LOG" is null
+//    public PostConstructTutorials() {
+//        LOG.info("Log info çağırıldı");
+//    }
+
+    // Bean henüz başlamadığından null pointer hatası alınıyor. Bu yüzden bağımlılığı enjekte edemiyoruz.
+    // Görevi: Bir bean nesnesi oluşturuldğunda hemen oluşur.
+    @PostConstruct
+    public void init() {
+        LOG.info("Log info çağırıldı");
+    }
+
+    public static void main(String[] args) {
+        PostConstructTutorials postConstructTutorials = new PostConstructTutorials();
+        System.out.println(postConstructTutorials);
+    }
+
+}
--- 3-IleriSeviye/Bolum01-JavaSpringBoot/SpringBoot/src/main/java/com/muazmemis/controller/BeanController.java
+++ 3-IleriSeviye/Bolum01-JavaSpringBoot/SpringBoot/src/main/java/com/muazmemis/controller/BeanController.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.annotation.PostConstruct;
+import java.util.logging.Logger;
+
 @Controller
 public class BeanController {
 
