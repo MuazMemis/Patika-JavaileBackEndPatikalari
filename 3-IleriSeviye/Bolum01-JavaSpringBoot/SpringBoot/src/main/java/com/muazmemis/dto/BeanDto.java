09286ff67d9d083679482f8f4fd39dfd9c2a0837
Muaz MEMİŞ <muazmemis@gmail.com>
2022-05-05 01:04:40 +0300

    SpringBoot 26 @Bean initMethod,detroyMethod,@Scope

--- 3-IleriSeviye/Bolum01-JavaSpringBoot/SpringBoot/src/main/java/com/muazmemis/dto/BeanDto.java
+++ 3-IleriSeviye/Bolum01-JavaSpringBoot/SpringBoot/src/main/java/com/muazmemis/dto/BeanDto.java
@@ -16,4 +16,17 @@ public class BeanDto {
     private Long id;
     private String beanName;
     private String beanData;
+
+    // Baslangic
+    public void initialBeanMethod() {
+        log.info("Bean başlamadan önce ben varım");
+        System.out.println("Bean başlamadan önce çalışacak method");
+    }
+
+    // Bitis
+    public void destroyBeanMethod() {
+        log.info("Bean bittikten sonra ben varım");
+        System.err.println("Bean bittikten sonra çalışacak method");
+    }
+
 }
