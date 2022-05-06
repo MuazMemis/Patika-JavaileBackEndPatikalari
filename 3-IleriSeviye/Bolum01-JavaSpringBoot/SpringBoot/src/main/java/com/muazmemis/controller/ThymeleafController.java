1d393766c644e0d73c6ecf68c936ca06436b560c
Muaz MEMİŞ <muazmemis@gmail.com>
2022-05-06 09:20:29 +0300

    SpringBoot 30 Model, thymeleaf, css, js

--- 3-IleriSeviye/Bolum01-JavaSpringBoot/SpringBoot/src/main/java/com/muazmemis/controller/ThymeleafController.java
+++ 3-IleriSeviye/Bolum01-JavaSpringBoot/SpringBoot/src/main/java/com/muazmemis/controller/ThymeleafController.java
@@ -1,8 +1,8 @@
 package com.muazmemis.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ThymeleafController {
@@ -13,4 +13,21 @@ public class ThymeleafController {
     public String getThymeleaf1() {
         return "thymeleaf1";
     }
+
+    // http://localhost:8080/thymeleaf2
+    @GetMapping("/thymeleaf2")
+    public String getThymeleaf2Model(Model model) {
+        model.addAttribute("key_model1", "Ben modelden geldim 1");
+        model.addAttribute("key_model2", "Ben modelden geldim 2");
+        return "thymeleaf1";
+    }
+
+    // http://localhost:8080/thymeleaf3
+    @GetMapping("/thymeleaf3")
+    public String getThymeleaf3Model(Model model) {
+        model.addAttribute("key_model1", "Ben modelden geldim 1");
+        model.addAttribute("key_model2", "Ben modelden geldim 2");
+        return "thymeleaf_file/thymeleaf3";
+    }
+
 }
