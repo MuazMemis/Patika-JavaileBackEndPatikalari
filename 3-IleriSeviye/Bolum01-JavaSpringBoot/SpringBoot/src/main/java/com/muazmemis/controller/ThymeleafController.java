666823d1aba2841147e843fee261445cabc0e483
Muaz MEMİŞ <muazmemis@gmail.com>
2022-05-06 12:44:27 +0300

    SpringBoot 32 Model object, Model List,Thymeleaf if ,else

--- 3-IleriSeviye/Bolum01-JavaSpringBoot/SpringBoot/src/main/java/com/muazmemis/controller/ThymeleafController.java
+++ 3-IleriSeviye/Bolum01-JavaSpringBoot/SpringBoot/src/main/java/com/muazmemis/controller/ThymeleafController.java
@@ -1,8 +1,12 @@
 package com.muazmemis.controller;
 
+import com.muazmemis.dto.ProductDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class ThymeleafController {
@@ -35,6 +39,36 @@ public class ThymeleafController {
     public String getThymeleaf4Model(Model model) {
         model.addAttribute("key_model1", "Ben modelden geldim 1");
         return "thymeleaf4";
+    }
+
+    // ---------------------------------------------------------------
+    // Model object göndermek
+    // http://localhost:8080/thymeleaf5
+    @GetMapping("/thymeleaf5")
+    public String getThymeleaf5ModelObject(Model model) {
+        model.addAttribute("key_model1", "text");
+
+        ProductDto productDto = ProductDto.builder().productId(0L).productName("Ürün adı").productPrice(2500).build();
+
+        model.addAttribute("key_model2", productDto);
+
+        return "thymeleaf5";
+    }
+
+    // http://localhost:8080/thymeleaf6
+    @GetMapping("/thymeleaf6")
+    public String getThymeleaf6ModelObjectList(Model model) {
+        model.addAttribute("key_model1", "text");
+
+        List<ProductDto> listem = new ArrayList<>();
+        listem.add(ProductDto.builder().productId(1L).productName("Ürün adı 1").productPrice(2500).build());
+        listem.add(ProductDto.builder().productId(2L).productName("Ürün adı 2").productPrice(3500).build());
+        listem.add(ProductDto.builder().productId(3L).productName("Ürün adı 3").productPrice(4500).build());
+        listem.add(ProductDto.builder().productId(4L).productName("Ürün adı 4").productPrice(5500).build());
+
+        model.addAttribute("product_liste", listem);
+
+        return "thymeleaf6";
     }
 
 }
---
+++ 3-IleriSeviye/Bolum01-JavaSpringBoot/SpringBoot/src/main/java/com/muazmemis/dto/BaseDto.java
@@ -0,0 +1,9 @@
+package com.muazmemis.dto;
+
+import java.util.Date;
+
+public class BaseDto {
+
+    private Long id;
+    private Date date;
+}
