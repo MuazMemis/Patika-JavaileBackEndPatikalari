678e7a2edfdbec0997bcf933317b7fa6621e4a5f
Muaz MEMİŞ <muazmemis@gmail.com>
2022-05-11 01:39:51 +0300

    SpringBoot 35 form @GetMapping @PostMapping @valid @Size @NotEmpty @Email

---
+++ 3-IleriSeviye/Bolum01-JavaSpringBoot/SpringBoot/src/main/java/com/muazmemis/dto/TeacherDto.java
@@ -0,0 +1,32 @@
+package com.muazmemis.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+import javax.validation.constraints.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Log4j2
+public class TeacherDto {
+
+    private Long teacherId;
+
+    @NotEmpty(message = "Ad Soyad alanı boş olamaz.")
+    @Size(min = 3, max = 50, message = "Ad Soyad alanı 3-50 karakter arasında olmalı.")
+    private String teacherNameSurname;
+
+    @Email(message = "Email adresi geçerli değil.")
+    @NotEmpty(message = "E-posta alanı boş olamaz.")
+    private String teacherEmail;
+
+    @NotEmpty(message = "Şifre alanı boş olamaz.")
+    @Size(min = 3, max = 8, message = "Şifre alanı 3-8 karakter arasında olmalı.")
+    private String teacherPassword;
+
+}
