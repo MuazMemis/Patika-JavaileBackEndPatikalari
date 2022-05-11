e1a58813aba097330bb8a34a3177d7ba2c5c16b3
Muaz MEMİŞ <muazmemis@gmail.com>
2022-05-11 19:40:12 +0300

    SpringBoot 41 Unit Test yazmak

---
+++ 3-IleriSeviye/Bolum01-JavaSpringBoot/SpringBoot/src/test/java/com/muazmemis/test/TestCrud.java
@@ -0,0 +1,14 @@
+package com.muazmemis.test;
+
+public interface TestCrud {
+
+    public void testCreate();
+
+    public void testUpdate();
+
+    public void testList();
+
+    public void testFindById();
+
+    public void testDelete();
+}
