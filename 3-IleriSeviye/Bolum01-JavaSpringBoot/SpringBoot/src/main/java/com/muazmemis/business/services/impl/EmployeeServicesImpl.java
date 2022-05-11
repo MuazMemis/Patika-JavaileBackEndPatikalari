3f6d3ac5040e2d50dfee83911f082ea23188e3d3
Muaz MEMİŞ <muazmemis@gmail.com>
2022-05-11 12:05:48 +0300

    SpringBoot 36 Part 1 Spring Data ,Spring Rest

---
+++ 3-IleriSeviye/Bolum01-JavaSpringBoot/SpringBoot/src/main/java/com/muazmemis/business/services/impl/EmployeeServicesImpl.java
@@ -0,0 +1,95 @@
+package com.muazmemis.business.services.impl;
+
+import com.muazmemis.business.dto.EmployeeDto;
+import com.muazmemis.business.services.EmployeeServices;
+import com.muazmemis.data.entity.EmployeeEntity;
+import com.muazmemis.data.repository.EmployoeeRepository;
+import com.muazmemis.exception.ResourceNotFoundException;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class EmployeeServicesImpl implements EmployeeServices {
+
+    @Autowired
+    private EmployoeeRepository employeeRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @GetMapping("/employees")
+    @Override
+    public List<EmployeeDto> getAllEmployees() {
+        List<EmployeeDto> listDto = new ArrayList<>();
+        Iterable<EmployeeEntity> entities = employeeRepository.findAll();
+        for (EmployeeEntity entity : entities) {
+            EmployeeDto employeeDto = entityToDto(entity);
+            listDto.add(employeeDto);
+        }
+        return listDto;
+    }
+
+    @PostMapping("/employees")
+    @Override
+    public EmployeeDto createEmployee(@RequestBody EmployeeDto employeeDto) {
+        EmployeeEntity employeeEntity = dtoToEntity(employeeDto);
+        employeeRepository.save(employeeEntity);
+        return employeeDto;
+    }
+
+    @GetMapping("/employees/{id}")
+    @Override
+    public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable Long id) throws Throwable {
+        EmployeeEntity employeeEntity = (EmployeeEntity) employeeRepository.findById(id).orElseThrow(() ->
+                new ResourceNotFoundException("Employee not exist with id: " + id));
+        EmployeeDto employeeDto = entityToDto(employeeEntity);
+
+        return ResponseEntity.ok(employeeDto);
+    }
+
+    @PutMapping("/employees/{id}")
+    @Override
+    public ResponseEntity<EmployeeDto> updateEmployee(@PathVariable Long id, @RequestBody EmployeeDto employeeDetails) throws Throwable {
+        EmployeeEntity employeeEntity = dtoToEntity(employeeDetails);
+        EmployeeEntity employee = (EmployeeEntity) employeeRepository.findById(id).orElseThrow(() ->
+                new ResourceNotFoundException("Employee not exist with id: " + id));
+        employee.setFirstName(employeeEntity.getFirstName());
+        employee.setLastName(employeeEntity.getLastName());
+        employee.setEmailId(employeeEntity.getEmailId());
+
+        EmployeeEntity updatedEmployee = (EmployeeEntity) employeeRepository.save(employee);
+        EmployeeDto employeeDto = entityToDto(updatedEmployee);
+        return ResponseEntity.ok(employeeDto);
+    }
+
+    @DeleteMapping("/employees/{id}")
+    @Override
+    public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable Long id) throws Throwable {
+        EmployeeEntity employeeEntity = (EmployeeEntity) employeeRepository.findById(id).orElseThrow(() ->
+                new ResourceNotFoundException("Employee not exist with id: " + id));
+        employeeRepository.delete(employeeEntity);
+
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return ResponseEntity.ok(response);
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    @Override
+    public EmployeeDto entityToDto(EmployeeEntity employeeEntity) {
+        return modelMapper.map(employeeEntity, EmployeeDto.class);
+    }
+
+    @Override
+    public EmployeeEntity dtoToEntity(EmployeeDto employeeDto) {
+        return modelMapper.map(employeeDto, EmployeeEntity.class);
+    }
+}
---
+++ 3-IleriSeviye/Bolum01-JavaSpringBoot/SpringBoot/src/main/java/com/muazmemis/data/repository/EmployoeeRepository.java
@@ -0,0 +1,9 @@
+package com.muazmemis.data.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+//JpaRepository>CrudRepository
+@Repository
+public interface EmployoeeRepository extends CrudRepository {
+}
