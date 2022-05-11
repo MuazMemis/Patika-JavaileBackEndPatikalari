1c811e5e7797c0d5ac1b11ec97266f7bfa71ee9c
Muaz MEMİŞ <muazmemis@gmail.com>
2022-05-11 16:41:57 +0300

    SpringBoot 40 Auditing

--- 3-IleriSeviye/Bolum01-JavaSpringBoot/SpringBoot/src/main/java/com/muazmemis/Application.java
+++ 3-IleriSeviye/Bolum01-JavaSpringBoot/SpringBoot/src/main/java/com/muazmemis/Application.java
@@ -2,7 +2,9 @@ package com.muazmemis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 @SpringBootApplication(exclude = {
         org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
         org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class}
