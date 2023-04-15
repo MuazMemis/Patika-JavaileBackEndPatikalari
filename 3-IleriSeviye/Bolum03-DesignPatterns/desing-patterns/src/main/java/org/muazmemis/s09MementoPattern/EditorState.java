1307c7804c3e72a4db1006d6564a6952d0172236
Muaz Memiş <muazmemis@gmail.com>
2023-04-15 12:20:32 +0300

    Memento pattern

---
+++ 3-IleriSeviye/Bolum03-DesignPatterns/desing-patterns/src/main/java/org/muazmemis/s09MementoPattern/EditorState.java
@@ -0,0 +1,16 @@
+package org.muazmemis.s09MementoPattern;
+
+public record EditorState(String content) {
+}
+
+//public class EditorState {
+//    private final String content;
+//
+//    public EditorState(String content) {
+//        this.content = content;
+//    }
+//
+//    public String getContent() {
+//        return content;
+//    }
+//}
